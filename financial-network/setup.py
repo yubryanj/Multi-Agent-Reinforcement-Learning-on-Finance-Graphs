63705247668975f3a6747c0f9caa01d6dacd774b
Bryan <bryany@kth.se>
2020-11-16 14:47:14 +0000

    Cleaning up setup

--- financial-network/setup.py
+++ financial-network/setup.py
@@ -2,5 +2,16 @@ from setuptools import setup
 
 setup(name='financial_network',
       version='0.0.1',
-      install_requires=['gym']
+      install_requires=['certifi==2020.6.20',
+                        'cloudpickle==1.2.0',
+                        'gym==0.15.4',
+                        'numpy==1.19.4',
+                        'opencv-python==4.4.0.46',
+                        'pandas==1.1.4',
+                        'Pillow==8.0.1',
+                        'pyglet==1.3.2',
+                        'scipy==1.5.4',
+                        'stable-baselines==2.10.1',
+                        'tensorflow==1.15.0'
+                        ]
 )
