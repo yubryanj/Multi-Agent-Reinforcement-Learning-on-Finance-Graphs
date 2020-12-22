Fixed a bug that was causing gradients not be back-progated from copying the original code.

multi_agent/agent.py
@@ -11,7 +11,7 @@ class Agent:
         """
         self.args = args
         self.agent_id = agent_identifier
-        self.policy = None  #TODO: Update this to mddpg
+        self.policy = MADDPG(args, agent_identifier)
 
 
     def select_action(self, observation, noise_rate, epsilon):
@@ -28,13 +28,29 @@ class Agent:
             action = np.random.uniform(-self.args.high_action, self.args.high_action, self.args.action_shape[self.agent_id])
         else:
             # Take the greedy step  
-            # TODO: Write me to do greedy action selection!
-            action = np.random.uniform(-self.args.high_action, self.args.high_action, self.args.action_shape[self.agent_id])
+
+            # Convert the observation vector into a tensor
+            inputs = torch.tensor( observation, dtype=torch.float32).unsqueeze(0)
+
+            # Gets the action from the actor given the observations
+            pi = self.policy.actor_network(inputs).squeeze(0)
+
+            # Concert the action tensor into an np array
+            action = pi.cpu().numpy()
+
+            # Generate some noise
+            noise = noise_rate + self.args.high_action * np.random.randn(*action.shape)
+
+            # Apply the noise
+            action += noise
+            
+            # Clip the action to the acceptable bounds
+            action = np.clip(action, -self.args.high_action, self.args.high_action)
 
         return action.copy()
 
 
-    def train(self, transitions, other_agents):
+    def learn(self, transitions, other_agents):
         if self.policy is not None:
         # Trains the agent
             self.policy.train(transitions, other_agents)

multi_agent/common/replay_buffer.py
@@ -13,7 +13,7 @@ class Buffer:
         self.buffer = dict()
 
         for agent_identifier in range(self.args.n_agents):
-            self.buffer[f'observation_{agent_identifier}'] = np.empty([self.max_size, self.args.obs_shape[agent_identifier]])
+            self.buffer[f'observations_{agent_identifier}'] = np.empty([self.max_size, self.args.obs_shape[agent_identifier]])
             self.buffer[f'actions_{agent_identifier}'] = np.empty([self.max_size, self.args.action_shape[agent_identifier]])
             self.buffer[f'rewards_{agent_identifier}'] = np.empty([self.max_size])
             self.buffer[f'next_observations_{agent_identifier}'] = np.empty([self.max_size, self.args.obs_shape[agent_identifier]])
@@ -21,7 +21,7 @@ class Buffer:
         self.lock = threading.Lock()
 
     
-    def store_episodes(self, observation, action, reward, next_observation):
+    def store_episode(self, observation, action, reward, next_observation):
         """
         Store the observation, action, reward, and next observation quadruple from the current episode
         :param  None
@@ -34,7 +34,7 @@ class Buffer:
         for agent_identifier in range(self.args.n_agents):
             # Retrieve the lock so that the data is updated simultaneously
             with self.lock:
-                self.buffer[f'observation_{agent_identifier}'][indices] = observation[agent_identifier]
+                self.buffer[f'observations_{agent_identifier}'][indices] = observation[agent_identifier]
                 self.buffer[f'actions_{agent_identifier}'][indices] = action[agent_identifier]
                 self.buffer[f'rewards_{agent_identifier}'][indices] = reward[agent_identifier]
                 self.buffer[f'next_observations_{agent_identifier}'][indices] = next_observation[agent_identifier]

multi_agent/policies/MADDPG/actor_critic.py
@@ -13,7 +13,7 @@ class Actor(nn.Module):
         self.max_action = args.high_action
         self.fc1 = nn.Linear(args.obs_shape[agent_identifier], 64)
         self.fc2 = nn.Linear(64,64)
-        self.gc3 = nn.Linear(64,64)
+        self.fc3 = nn.Linear(64,64)
         self.action_out = nn.Linear(64, args.action_shape[agent_identifier])
 
     def forward(self, x):
@@ -30,7 +30,7 @@ class Actor(nn.Module):
 
 class Critic(nn.Module):
     def __init__(self, args) -> None:
-        super(Critic).__init__()
+        super(Critic, self).__init__()
         self.max_action = args.high_action
         self.fc1 = nn.Linear(sum(args.obs_shape) + sum(args.action_shape), 64)
         self.fc2 = nn.Linear(64,64)

multi_agent/policies/MADDPG/maddpg.py
@@ -16,6 +16,10 @@ class MADDPG:
         self.actor_target_network = Actor(args, agent_identifier)
         self.critic_target_network = Critic(args)
 
+        # load the weights into the target network -- both networks initializes to same place
+        self.actor_target_network.load_state_dict(self.actor_network.state_dict())
+        self.critic_target_network.load_state_dict(self.critic_network.state_dict())
+
         # Create the optimizer
         self.actor_optimizer = torch.optim.Adam(self.actor_network.parameters(), lr=self.args.lr_actor)
         self.critic_optimizer = torch.optim.Adam(self.critic_network.parameters(), lr=self.args.lr_critic)
@@ -57,7 +61,7 @@ class MADDPG:
             target_param.data.copy_((1-self.args.tau) * target_param.data + self.args.tau * param.data)
 
         # Update the critic network
-        for target_param, param in zip(self.critic_target.parameters(), self.critic_network.parameters()):
+        for target_param, param in zip(self.critic_target_network.parameters(), self.critic_network.parameters()):
             target_param.data.copy_((1-self.args.tau) * target_param.data + self.args.tau * param.data)
 
 
@@ -73,7 +77,7 @@ class MADDPG:
             transitions[key] = torch.tensor(transitions[key], dtype=torch.float32)
         
         # Retrieve the rewards
-        rewards = transitions[f'r_{self.agent_id}']
+        rewards = transitions[f'rewards_{self.agent_identifer}']
         
         # Allocate lists to store the observation, action, and next observation triplets
         observations, actions, next_observations = [], [], []
@@ -81,8 +85,8 @@ class MADDPG:
         # Store the triplet for each agent in the given transition
         for agent_identifier in range(self.args.n_agents):
             observations.append(transitions[f'observations_{agent_identifier}'])
-            actions.append(transitions[f'actions{agent_identifier}'])
-            next_observations.append(transitions[f'next_observations{agent_identifier}'])
+            actions.append(transitions[f'actions_{agent_identifier}'])
+            next_observations.append(transitions[f'next_observations_{agent_identifier}'])
 
         # Calculate the target Q Value
         next_action = []
@@ -114,7 +118,7 @@ class MADDPG:
         actions[self.agent_identifer] = self.actor_network(observations[self.agent_identifer])
         
         # Calcu
-        actor_loss = - self.critic_ntwork(observations, actions).mean()
+        actor_loss = - self.critic_network(observations, actions).mean()
 
         # Clear the actor gradients for back propagation
         self.actor_optimizer.zero_grad()
@@ -146,6 +150,7 @@ class MADDPG:
         Save the model
         :param  train_step  current training step, used for naming
         """
+        print("Saving model")
 
         num = str(train_step // self.args.save_rate)
         model_path = os.path.join(self.args.save_dir, self.args.scenario_name)